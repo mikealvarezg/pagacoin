f2318ddf63a88c966aad1a8b0990995020ca7a0b
Miguel Álvarez Granado <miguel.alvarez.granado@gmail.com>
2021-02-22 19:58:13 +0100

    Swagger

--- pagacoin-api/src/main/java/com/pagantis/pagacoin/controller/TransactionController.java
+++ pagacoin-api/src/main/java/com/pagantis/pagacoin/controller/TransactionController.java
@@ -2,11 +2,17 @@ package com.pagantis.pagacoin.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import com.pagantis.pagacoin.model.api.TransactionApi;
+import com.pagantis.pagacoin.model.api.UserApi;
 import com.pagantis.pagacoin.service.TransactionService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,12 +29,27 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @GetMapping("/transactions")
+    @ApiResponses({ //
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "OK", response = TransactionApi[].class),
+            @ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "UNAUTHORIZED"),
+            @ApiResponse(code = HttpServletResponse.SC_FORBIDDEN, message = "FORBIDDEN"),
+            @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "NOT FOUND"),
+    })
+    @ApiOperation(value = "Get the application list of transactions")
     public List<TransactionApi> getTransactions() {
         return transactionService.getTransactions();
     }
 
     @PostMapping("/transactions")
-    public ResponseEntity<Object> createTransaction(@Valid @RequestBody TransactionApi transactionApi) {
+    @ApiResponses({ //
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "OK"),
+            @ApiResponse(code = HttpServletResponse.SC_CREATED, message = "CREATED"),
+            @ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "UNAUTHORIZED"),
+            @ApiResponse(code = HttpServletResponse.SC_FORBIDDEN, message = "FORBIDDEN"),
+            @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "NOT FOUND"),
+    })
+    @ApiOperation(value = "Creates a transaction")
+    public ResponseEntity<Object> createTransaction(@ApiParam(value = "Transaction" , name = "transaction", required = true) @Valid @RequestBody TransactionApi transactionApi) {
         return transactionService.createTransaction(transactionApi);
     }
 }
--- pagacoin-api/src/main/java/com/pagantis/pagacoin/controller/UserController.java
+++ pagacoin-api/src/main/java/com/pagantis/pagacoin/controller/UserController.java
@@ -1,17 +1,20 @@
 package com.pagantis.pagacoin.controller;
 
-import java.util.List;
-
 import com.pagantis.pagacoin.model.api.UserApi;
-import com.pagantis.pagacoin.model.entity.User;
 import com.pagantis.pagacoin.model.entity.Wallet;
 import com.pagantis.pagacoin.service.UserService;
-
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -20,12 +23,26 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users")
+    @ApiResponses({ //
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "OK", response = UserApi[].class),
+            @ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "UNAUTHORIZED"),
+            @ApiResponse(code = HttpServletResponse.SC_FORBIDDEN, message = "FORBIDDEN"),
+            @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "NOT FOUND"),
+    })
+    @ApiOperation(value = "Get the application list of users")
     public List<UserApi> getUsers() {
         return userService.getUsers();
     }
 
     @GetMapping("/users/{id}/wallets")
-    public List<Wallet> getWalletsByUser(@PathVariable Integer id) {
+    @ApiResponses({ //
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "OK", response = Wallet[].class),
+            @ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "UNAUTHORIZED"),
+            @ApiResponse(code = HttpServletResponse.SC_FORBIDDEN, message = "FORBIDDEN"),
+            @ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "NOT FOUND"),
+    })
+    @ApiOperation(value = "Get the wallet list of a user")
+    public List<Wallet> getWalletsByUser(@ApiParam(value = "User id" , name = "id", required = true) @PathVariable Integer id) {
         return userService.getWalletsByUser(id);
     }
 
